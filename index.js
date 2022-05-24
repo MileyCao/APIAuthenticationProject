f0898dd276b3d388f433f68818ed50466c0540c7
MileyCao <cao4@ualberta.ca>
2022-05-24 00:50:03 -0600

    set up express

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, ()=> console.log('Server up and running'));
+const authRoute = require('./routes/auth');
 
+//Route Middleware
+app.use('/api/user', authRoute);
+
+app.listen(3000, () => console.log('Server up and running'));
