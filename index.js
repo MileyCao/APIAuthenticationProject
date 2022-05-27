45992f1f130625ccc197d9ad4f9fadee3fc9ca09
MileyCao <cao4@ualberta.ca>
2022-05-27 00:55:03 -0600

    connect to mongodb

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+//connect to DB
+mongoose.connect(
+  'mongodb+srv://ricecake:Cy324568@cluster0.fe6qu.mongodb.net/?retryWrites=true&w=majority',
+  () => console.log('connect to db!')
+);
 
 const authRoute = require('./routes/auth');
 
