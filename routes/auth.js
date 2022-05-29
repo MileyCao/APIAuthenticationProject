ceb8df6a0c9b72042a63a6a1d146b8cc2ea05fc4
MileyCao <cao4@ualberta.ca>
2022-05-28 18:58:44 -0600

    validate user info with Joi

--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,32 @@
 ///const res = require('express/lib/response');
 const User = require('../model/User');
-
 const router = require('express').Router();
 
+//Validation
+const Joi = require('@hapi/joi');
+
+const schema = Joi.object({
+  name: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(6).required(),
+});
+
 router.post('/register', async (req, res) => {
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
-  try {
-    const savedUser = await user.save();
-    res.send(savedUser);
-  } catch (err) {
-    res.status(400).send(err);
-  }
+  //before making a user, validate here
+  const { error } = schema.validate(req.body);
+  res.send(error.details[0].message);
+
+  // const user = new User({
+  //   name: req.body.name,
+  //   email: req.body.email,
+  //   password: req.body.password,
+  // });
+  // try {
+  //   const savedUser = await user.save();
+  //   res.send(savedUser);
+  // } catch (err) {
+  //   res.status(400).send(err);
+  // }
 });
 
 // router.post('/login');
