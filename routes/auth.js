d215a16cfb61df011e5a8f498096b5d3bfdf9f53
MileyCao <cao4@ualberta.ca>
2022-05-28 23:25:16 -0600

    hashing password

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,9 @@
-///const res = require('express/lib/response');
-const req = require('express/lib/request');
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
 const router = require('express').Router();
 const { registerValidation, loginValidation } = require('../validation');
 
+//REGISTER
 router.post('/register', async (req, res) => {
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
@@ -12,14 +12,19 @@ router.post('/register', async (req, res) => {
   const emailExist = await User.findOne({ email: req.body.email });
   if (emailExist) return res.status(400).send('Email already exists');
 
+  //Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   });
   try {
     const savedUser = await user.save();
-    res.send(savedUser);
+    // res.send(savedUser); not a good idea to send hashing pwd
+    res.send({ user: user._id });
   } catch (err) {
     res.status(400).send(err);
   }
