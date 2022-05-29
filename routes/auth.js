78ca30fe21b677a2a7950aba7882f4be65430da0
MileyCao <cao4@ualberta.ca>
2022-05-28 23:34:48 -0600

    set up log in

--- routes/auth.js
+++ routes/auth.js
@@ -30,6 +30,20 @@ router.post('/register', async (req, res) => {
   }
 });
 
-// router.post('/login');
+//LOGIN
+router.post('/login', async (req, res) => {
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //check if the email existed
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send('Email does not exist');
+
+  //check pwd match
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send('Invalid Password');
+
+  res.send('Succesfully Logged In!');
+});
 
 module.exports = router;
